8e6431299a1b57a60c069ba86845d3cc90543caa
metacollin <metacollin@gmail.com>
2014-12-28 11:39:06 -0700

    brew audit fixes

--- kicad.rb
+++ kicad.rb
@@ -8,48 +8,44 @@ class Kicad < Formula
     url "http://downloads.sourceforge.net/project/wxpython/wxPython/3.0.2.0/wxPython-src-3.0.2.0.tar.bz2"
     sha1 "5053f3fa04f4eb3a9d4bfd762d963deb7fa46866"
   end
-  
+
   depends_on "cmake" => :build
-  depends_on "boost" 
+  depends_on "boost"
   depends_on "cairo"
   depends_on "swig" => :build
   depends_on "pkg-config"
   depends_on "libpng"
-  depends_on "pcre"  
+  depends_on "pcre"
 
   def install
-    kicad_path = buildpath
-
     resource("wxPython").stage do
       (buildpath/"wx").install Dir["*"]
       #system "cd #{buildpath}"
       cd buildpath/"wx" do
-        system "patch", "-p0", "-i#{kicad_path}/patches/wxwidgets-3.0.0_macosx.patch"
-        system "patch", "-p0", "-i#{kicad_path}/patches/wxwidgets-3.0.0_macosx_bug_15908.patch"
-        system "patch", "-p0", "-i#{kicad_path}/patches/wxwidgets-3.0.0_macosx_soname.patch"
-        #if MacOS.version >= :yosemite
+        system "patch", "-p0", "-i#{buildpath}/patches/wxwidgets-3.0.0_macosx.patch"
+        system "patch", "-p0", "-i#{buildpath}/patches/wxwidgets-3.0.0_macosx_bug_15908.patch"
+        system "patch", "-p0", "-i#{buildpath}/patches/wxwidgets-3.0.0_macosx_soname.patch"
         system "curl https://gist.githubusercontent.com/metacollin/c138f049e41d9cc42ef9/raw/0174436d883d5eebdbad18a8d98e8d318df59725/patch | patch -p0"
-        #end
+
         ENV['CC'] = "/usr/bin/clang"
         ENV['CXX'] = "/usr/bin/clang++"
+        
         args = %W[
-          --prefix=#{kicad_path}/wx-bin 
-          --with-opengl 
-          --enable-aui 
-          --enable-utf8 
-          --enable-html 
-          --enable-stl 
-          --with-libjpeg=builtin 
-          --with-libpng=builtin 
-          --with-regex=builtin 
-          --with-libtiff=builtin 
-          --with-zlib=builtin 
-          --with-expat=builtin 
-          --without-liblzma 
-          --with-macosx-version-min=#{MacOS.version} 
+          --prefix=#{kicad_path}/wx-bin
+          --with-opengl
+          --enable-aui
+          --enable-utf8
+          --enable-html
+          --enable-stl
+          --with-libjpeg=builtin
+          --with-libpng=builtin
+          --with-regex=builtin
+          --with-libtiff=builtin
+          --with-zlib=builtin
+          --with-expat=builtin
+          --without-liblzma
+          --with-macosx-version-min=#{MacOS.version}
           --enable-universal-binary=i386,x86_64
-          CC=clang 
-          CXX=clang++
           ]
         system "mkdir", "wx-build"
         cd "wx-build" do
@@ -76,7 +72,7 @@ class Kicad < Formula
 #    cd "build" do
         ENV["CMAKE_C_COMPILER"] = "/usr/bin/clang"
         ENV["CMAKE_CXX_COMPILER"] = "/usr/bin/clang++"
-      
+
         args = %W[
           -DCMAKE_INSTALL_PREFIX=#{prefix}
           -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
