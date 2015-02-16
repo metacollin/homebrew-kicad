1b2f65e24a2f63be26604dcfc36ac816985fcc9c
metacollin <metacollin@gmail.com>
2015-02-16 02:06:39 -0700

    Totally reworked.

--- kicad.rb
+++ kicad.rb
@@ -4,40 +4,54 @@ class Kicad < Formula
   homepage "http://kicad-pcb.org"
   head "https://code.launchpad.net/~kicad-product-committers/kicad/product", :using => :bzr
 
+  #`env :std
+  
   depends_on "bzr" => :build
   depends_on "cmake" => :build
-  depends_on "cairo"
-  depends_on "swig" 
-  depends_on "pkg-config"
-  depends_on "libpng"
-  depends_on "pcre"
-  depends_on "glew"
   depends_on "kicad-library" => :recommended
   depends_on "wxkicad" 
+  #epends_on "wxkpython"
+  depends_on "swig" => :build
+  depends_on "pcre" => :build
+
+  patch :p0 do
+    url "https://gist.githubusercontent.com/metacollin/97b547034d144f483c0f/raw/8d7f4fc2b119b126bb76322e5c1461f61bf37ef7/boost.patch"
+    sha1 "e3a5b58219123e69a569f4b68fa5ee1acf356b00"
+  end
 
 
   def install
       mkdir "build" do
-          ENV["CMAKE_C_COMPILER"] = "/usr/bin/clang"
-          ENV["CMAKE_CXX_COMPILER"] = "/usr/bin/clang++"
-
+          #ENV["CMAKE_C_COMPILER"] = "/usr/bin/clang"
+          #ENV["CMAKE_CXX_COMPILER"] = "/usr/bin/clang++"
+         # ENV.append "CPPFLAGS", "-I/usr/local/opt/wxkicad/include"
+         # ENV.append "LDFLAGS", " -L/usr/local/opt/wxkicad/lib"
+         #ENV.delete "CPPFLAGS"
+         #ENV.delete "LDFLAGS"
+        # ENV.delete "CMAKE_PREFIX_PATH"
+        # ENV.delete "ALOCAL_PATH"
+        ENV.prepend_create_path "PYTHONPATH", "#{Formula["wxkicad"].lib}/python2.7/site-packages"
+        ENV.libcxx
+        
           args = %W[
+            -DCMAKE_C_COMPILER=/usr/bin/clang
+            -DCMAKE_CXX_COMPILER=/usr/bin/clang++
             -DCMAKE_INSTALL_PREFIX=#{prefix}
             -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
-            -DwxWidgets_CONFIG_EXECUTABLE=#{HOMEBREW_PREFIX}/opt/wxkicad/bin/wx-config
+            -DwxWidgets_CONFIG_EXECUTABLE=#{Formula["wxkicad"].bin}/wx-config
             -DPYTHON_EXECUTABLE=/usr/bin/python
             -DPYTHON_LIBRARY=/usr/lib/libpython.dylib
-            -DPYTHON_SITE_PACKAGE_PATH=#{HOMEBREW_PREFIX}/opt/wxkicad/lib/python2.7/site-packages
+            -DPYTHON_SITE_PACKAGE_PATH=#{Formula["wxkicad"].lib}/python2.7/site-packages
             -DKICAD_SCRIPTING=ON
             -DKICAD_SCRIPTING_MODULES=ON
             -DKICAD_SCRIPTING_WXPYTHON=ON
             -DCMAKE_BUILD_TYPE=Release
+            -DCMAKE_CXX_FLAGS=-stdlib=libc++
+            -DCMAKE_C_FLAGS=-stdlib=libc++
         ]
 
-
-
           system "cmake", "../", *args
-          system "make -j8"
+          system "make", "-j8"
           system "make install"
         end
       end
--- wxkicad.rb
+++ wxkicad.rb
@@ -1,21 +1,13 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Wxkicad < Formula
-  homepage ""
-  url "http://downloads.sourceforge.net/project/wxpython/wxPython/3.0.2.0/wxPython-src-3.0.2.0.tar.bz2"
-  version "3.0.2.0"
+  url "https://downloads.sourceforge.net/project/wxpython/wxPython/3.0.2.0/wxPython-src-3.0.2.0.tar.bz2"
   sha1 "5053f3fa04f4eb3a9d4bfd762d963deb7fa46866"
 
-  depends_on "cairo"
-  depends_on "swig" 
-  depends_on "pkg-config"
-  depends_on "libpng"
-  depends_on "pcre"
-  depends_on "glew"
-
- # option "without-wxpython", "Only build wx for kicad, but not wxpython for kicad.  wxpython is required for python scripting."
+  depends_on "cairo" => [:universal, "without-x11"] 
+  depends_on "swig" => :build
+  depends_on "pkg-config" => :build
+  depends_on "pcre" => :universal
+  depends_on "glew" => :universal
+  dpeends_on "expat" => :universal
 
   keg_only "Custom patched version of wx and wxPython, only for use by KiCad."
 
@@ -24,59 +16,59 @@ class Wxkicad < Formula
     sha1 "147d38994c58d305b0b45b822fe2d18e7390c6c5"
   end
 
-
-
   def install
-    ENV['CC'] = "/usr/bin/clang"
-    ENV['CXX'] = "/usr/bin/clang++"
-    unless MacOS.version < :mavericks
-      ENV.append "CXXFLAGS", "-stdlib=libc++"
-      ENV.append "ARCHFLAGS", "-Wno-error=unused-command-line-argument-hard-error-in-future"
-    else
-      ENV.append "CXXFLAGS", "-stdlib=libstdc++"
-    end
-    
-    args = %W[
-      --prefix=#{prefix}
-      --with-opengl
-      --enable-aui
-      --enable-utf8
-      --enable-html
-      --enable-stl
-      --with-libjpeg=builtin
-      --with-libpng=builtin
-      --with-regex=builtin
-      --with-libtiff=builtin
-      --with-zlib=builtin
-      --with-expat=builtin
-      --without-liblzma
-      --with-macosx-version-min=#{MacOS.version}
-      --enable-universal-binary=i386,x86_64
+    ENV['MAC_OS_X_VERSION_MIN_REQUIRED'] = "#{MacOS.version}"
+    ENV.libcxx
+
+    args = [
+      "--prefix=#{prefix}",
+      "--with-opengl",
+      "--enable-aui",
+      "--enable-utf8",
+      "--enable-html",
+      "--enable-stl",
+      "--with-libjpeg=builtin",
+      "--with-libpng=builtin",
+      "--with-regex=builtin",
+      "--with-libtiff=builtin",
+      "--with-zlib=builtin",
+      "--with-expat=builtin",
+      "--without-liblzma",
+      "--with-macosx-version-min=#{MacOS.version}",
+      "--enable-universal_binary=i386,x86_64",
+      "CC=/usr/bin/clang",
+      "CXX=/usr/bin/clang++"
       ]
-    
-    
+     
     system "./configure", *args
-    system "make", "-j8"
-    system "make", "install"
+    system "make", "-j6", "install"
 
+    ohai ""
+    ohai "*****"
     ohai "The custom KiCad version of wx has built successfully. Woohoo!"
+    ohai "*****"
+    ohai "But we're not done yet.  Hold onto your butts, now building wxpython."
 
-   # unless build.without? "wxpython"
     cd "wxPython" do
-        ohai "...But we're not out of the woods yet.  Now building wxPython."
-        opoo "In the words of Samuel L. Jackson, hold onto your butt."
-        ENV['CC'] = "/usr/bin/clang"
-        ENV['CXX'] = "/usr/bin/clang++"
-        unless MacOS.version < :mavericks
-          ENV.append "CXXFLAGS", "-stdlib=libc++"
-          ENV.append "ARCHFLAGS", "-Wno-error=unused-command-line-argument-hard-error-in-future"
-        else
-          ENV.append "CXXFLAGS", "-stdlib=libstdc++"
-        end
+     ENV['MAC_OS_X_VERSION_MIN_REQUIRED'] = "#{MacOS.version}"
+     ENV.libcxx
+     ENV["WXWIN"] = buildpath
 
-        system "/usr/bin/python", "setup.py", "build_ext", "WX_CONFIG=#{opt_prefix}/bin/wx-config", "UNICODE=1", "WXPORT=osx_cocoa", "BUILD_BASE=../"
-        system "/usr/bin/python", "setup.py", "install", "--prefix=#{prefix}", "WX_CONFIG=#{opt_prefix}/bin/wx-config", "UNICODE=1", "WXPORT=osx_cocoa", "BUILD_BASE=../"
-     # end
+     blargs = [
+      "WXPORT=osx_cocoa",
+      "WX_CONFIG=#{bin}/wx-config",
+      "UNICODE=1",
+      "BUILD_BASE=#{buildpath}"
+     ]
+     system "/usr/local/python", "setup.py",
+                     "build_ext",
+                     *blargs
+
+     system "/usr/local/python", "setup.py",
+                     "install",
+                     "--prefix=#{prefix}",
+                     *blargs
     end
+    system "chmod", "-R", "0744", "#{lib}"
   end
 end
